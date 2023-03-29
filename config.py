5157d937ab6d9d52d1b403dc5a348bc00d1a5741
Yifei Liu <liu1456447215@gmail.com>
2023-03-29 14:45:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-# API_KEY = "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" 此key无效
-API_KEY = "sk-此处填API秘钥"
+import os
+
+API_KEY = os.getenv("OPENAIKEY")
 API_URL = "https://api.openai.com/v1/chat/completions"
 
 # 改为True应用代理
